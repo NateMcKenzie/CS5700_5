450189065b6525fe08ade60870d7b819de92750a
Nate McKenzie <nate6264@gmail.com>
2024-08-08 19:11:21 -0600

    Implement FloatDetector

--- src/main/kotlin/FloatDetector.kt
+++ src/main/kotlin/FloatDetector.kt
@@ -1,2 +1,6 @@
-class FloatDetector {
+import floatStates.FirstDigit
+import floatStates.Valid
+
+class FloatDetector : Detector(FirstDigit()) {
+    override fun checkFinalState() = state is Valid
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,27 @@
 fun main() {
-    val detectors = arrayOf(IntegerDetector())
-    var running = true
-    while (running){
+    val detectors = arrayOf(IntegerDetector(), FloatDetector())
+
+    while (true){
         print("""
             What detector?
-            
-            0) Integer
-            1) Float
-            2) Binary
-            3) Email
-            4) Password
+            =========================
+                0) Integer
+                1) Float
+                2) Binary
+                3) Email
+                4) Password
+                5) Quit
+            =========================
             
         """.trimIndent())
         val index = readln().toInt()
+        if(index == 5) break
+
         println("Enter String:")
         val input = readln()
+
         val response = detectors[index].detect(input)
+
         println(if (response) "Valid" else "Invalid")
     }
 }
